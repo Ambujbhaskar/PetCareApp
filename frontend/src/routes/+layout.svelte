4a963f6acd473bb136a7f3b55b63548c74f14633
Karan Singh <karan.mnj.singh@gmail.com>
2023-03-28 02:10:46 +0530

    add navbar and header

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,19 +1,14 @@
 <script>
 	import Header from './Header.svelte';
-	import './styles.css';
-	import "../app.css";
+	import Navbar from './Navbar.svelte';
 </script>
 
 <div class="app">
 	<Header />
-
 	<main>
 		<slot />
 	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	<Navbar/>
 </div>
 
 <style>
@@ -32,23 +27,5 @@
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
 	}
 </style>
---
+++ frontend/src/routes/Navbar.svelte
@@ -0,0 +1,75 @@
+<script>
+	let activeButton = "home";
+	let state = {
+		"home" : "active",
+		"vaccine" : "idle",
+		"blogs" : "idle",
+		"sos" : "idle",
+	};
+
+	function changeState(button) {
+		if (button === activeButton)
+			return;
+		state[button] = "active";
+		state[activeButton] = "idle";
+		activeButton = button;
+	}
+</script>
+
+<nav class="navbar">
+	<div>
+		<img 
+			src={"home-" + state["home"] + ".svg"} 
+			alt="home button" 
+			on:click={() => {changeState("home")}}
+		>
+		Home
+	</div>
+	<div>
+		<img 
+			src={"vaccine-" + state["vaccine"] + ".svg"} 
+			alt="vaccine button" 
+			on:click={() => {changeState("vaccine")}}
+		>
+		Vaccine
+	</div>	
+	<div>
+		<img 
+			src={"blogs-" + state["blogs"] + ".svg"} 
+			alt="blogs button" 
+			on:click={() => {changeState("blogs")}}
+		>
+		Blogs
+	</div>	
+	<div>
+		<img 
+			src={"sos-" + state["sos"] + ".svg"} 
+			alt="sos button" 
+			on:click={() => {changeState("sos")}}
+		>
+		SOS
+	</div>
+</nav>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200;0,300;0,400;0,500;1,200;1,300;1,400;1,500&display=swap');
+
+	.navbar {
+		display: flex;
+		justify-content: space-between;
+		padding: 1rem 2rem 1.4rem 2rem;
+	}
+	.navbar div {
+		display: flex;
+		flex-flow: column;
+		justify-content: center;
+		align-items: center;
+		font-family: 'Karla', sans-serif;
+		font-size: 1rem;
+		gap: 2px;
+	}
+
+	.navbar div img {
+		height: 2rem;
+	}
+</style>
