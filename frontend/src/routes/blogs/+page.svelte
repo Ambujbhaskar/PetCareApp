9bdb1618393ce76e4e78109a9aa8dfa88c9c1339
Kritarth384 <kritarth20384@iiitd.ac.in>
2023-04-04 10:11:27 +0530

    Update in Blog Section  - search category ...

--- frontend/src/routes/blogs/+page.svelte
+++ frontend/src/routes/blogs/+page.svelte
@@ -43,6 +43,18 @@
   let selectedProfile = 0;
   export let saved;
   export let bookMarked;
+  export let search;
+  let tabs = [
+    { name: "CATS", active: false },
+    { name: "NUTRITION", active: true },
+    { name: "DOGS", active: false },
+    { name: "VETS", active: false },
+    { name: "VACCINATION", active: false },
+  ];
+
+  function setActiveTab(index) {
+    tabs = tabs.map((tab, i) => ({ ...tab, active: i === index }));
+  }
 
   function bookMarked_op() {
     bookMarked = !bookMarked;
@@ -57,34 +69,102 @@
 <section>
   <div>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div class="flex">
-      <div
-        class={`border-black rounded-[20px] border-2 w-[12rem] h-[3rem] flex px-5 justify-between items-center cursor-pointer mr-2
+    <div class="flex justify-between items-center">
+      {#if !search}
+        <div class="flex">
+          <div
+            class={`border-black rounded-[20px] border-[0.13rem] w-[12rem] h-[3rem] flex px-5 justify-between 
+            items-center cursor-pointer mr-2
       ${bookMarked && "bg-[#F2F4D1]"}`}
-        on:click={bookMarked_op}
-      >
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <img
-          src={bookMarked
-            ? "./bookmark-checked.svg"
-            : "./bookmark-unchecked.svg"}
-          alt="Icon"
-          on:click={bookMarked_op}
-        />
-        View Bookmarks
-      </div>
-      {#if bookMarked}
-        <img
-          src={bookMarked && "./delete-bmk.svg"}
-          alt="Icon"
+            on:click={() => {
+              bookMarked = true;
+            }}
+          >
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <img
+              src={bookMarked
+                ? "./bookmark-checked.svg"
+                : "./bookmark-unchecked.svg"}
+              alt="Icon"
+              on:click={() => {
+                bookMarked = true;
+              }}
+            />
+            View Bookmarks
+          </div>
+          {#if bookMarked}
+            <img
+              src={bookMarked && "./delete-bmk.svg"}
+              alt="Icon"
+              on:click={() => {
+                bookMarked = false;
+              }}
+              class="cursor-pointer transition duration-800 ease-in-out delay-700"
+            />
+          {/if}
+        </div>
+      {:else}
+        <div
+          class={`border-black rounded-full border-2 h-[3rem] flex px-5 justify-between items-center cursor-pointer mr-2
+  ${bookMarked && "bg-[#F2F4D1]"}`}
           on:click={() => {
-            bookMarked = !bookMarked;
+            bookMarked = true;
+            search = false;
           }}
-          class="cursor-pointer"
-        />
+        >
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
+          <img
+            src={bookMarked
+              ? "./bookmark-checked.svg"
+              : "./bookmark-unchecked.svg"}
+            alt="Icon"
+            on:click={() => {
+              bookMarked = true;
+            }}
+          />
+        </div>
+      {/if}
+
+      {#if !search}
+        <div class="transition duration-800 ease-in-out">
+          <img
+            src={"./search.svg"}
+            alt="Search"
+            class="cursor-pointer"
+            on:click={() => {
+              search = true;
+            }}
+          />
+        </div>
+      {:else}
+        <div
+          class="flex items-center border-black border-2 w-[25rem] rounded-[20px]  h-[3rem] px-5 
+          transition duration-2000 ease-in-out"
+        >
+          <img src="./search_icon.svg" alt="Search" class="mr-4" />
+          <input
+            type="text"
+            placeholder="Search"
+            class="outline-none w-[20rem]"
+          />
+        </div>
       {/if}
     </div>
-    <h4>Suggested Articles</h4>
+    <div class="flex justify-between items-center m-2 mt-6 mb-6">
+      {#each tabs as tab, i}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div
+          class={`flex justify-center items-center px-3 rounded-[0.5rem]  
+          border-[0.124rem] border-[#5E6073] cursor-pointer text-[0.9rem] font-medium ${
+            tab.active ? "bg-[#5E6073] text-white" : "text-[#5E6073]"
+          }`}
+          class:selected={tab.active}
+          on:click={() => setActiveTab(i)}
+        >
+          {tab.name}
+        </div>
+      {/each}
+    </div>
     <SuggestedArticles />
   </div>
 </section>
